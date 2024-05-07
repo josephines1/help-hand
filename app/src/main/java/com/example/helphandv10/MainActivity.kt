508d5f88150f3c3a498104327f59f4ed475ebe70
josephines1 <josephine.bless777@gmail.com>
2024-05-07 11:19:44 +0700

    Add halaman home (sebagian)

--- app/src/main/java/com/example/helphandv10/MainActivity.kt
+++ app/src/main/java/com/example/helphandv10/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.helphandv10
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.helphandv10.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        replaceFragment(HomeFragment())
 
 
 //        enableEdgeToEdge()
@@ -15,5 +21,14 @@ class MainActivity : AppCompatActivity() {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager
+            .beginTransaction()
+
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
